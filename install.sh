94d742c540bc2584c54a472ad876ec0e4b7ebc82
atulsingh0 <3430610+atulsingh0@users.noreply.github.com>
2023-05-24 00:09:29 -0400

    main | updated install script

--- install.sh
+++ install.sh
@@ -3,6 +3,37 @@
 # Setup script for setting up a new macos/linux machine
 echo "Starting setup"
 
+if [ "$#" -ne 2 ]; then
+    echo "$(tput setaf 228)Enter your macOS home directory (usually: /Users/username)$(tput sgr0)"
+    read -p "Enter your home directory: " user_homedir
+    echo -e "\n"
+    echo "$(tput setaf 228)Enter the relative path to your dotfiles directory $(tput sgr0)"
+    echo "$(tput setaf 228)Example: if the full path is $(tput setaf 15)'/Users/username/dotfiles'$(tput setaf 228), type $(tput setaf 15)'dotfiles'$(tput sgr0)"
+    echo "$(tput setaf 228)(defaults to current directory if empty)$(tput sgr0)"
+    read -p "Enter the relative path to your dotfiles directory: " user_dotfiles_dir
+    if [ -z "$user_dotfiles_dir" ]; then
+        absolute_curr=$PWD
+        user_dotfiles_dir=${absolute_curr#$user_homedir}
+    fi
+else
+    user_homedir=$1
+    user_dotfiles_dir=$2
+fi
+
+export homedir=$user_homedir
+
+# dotfiles directory
+export dotfiledir=${homedir}/${user_dotfiles_dir}
+
+
+
+# Setting up OH-MY-ALIASES
+git clone https://github.com/atulsingh0/oh-my-aliases.git ~/.ohmyaliases
+
+# Add below line in your bashrc/zshrs/kshrc file
+source ~/.ohmyaliases/source_aliases.sh
+
+# Setting Up MAC
 if [ "$(uname)" = "Darwin" ]; then
     echo "Detecting MacOS"
     # calling macos script
--- mac-setup.sh
+++ mac-setup.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 # install xcode CLI
-xcode-select --install
+if command -v xcode-select; then
+    echo "Installing xcode-selct..."
+    xcode-select --install
+fi
 
 # Check for Homebrew to be present, install if it's missing
 if command -v brew; then
@@ -9,5 +12,20 @@ if command -v brew; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
+# list of files/folders to symlink in ${homedir}
+files="zprofile zshrc"
+
+# change to the dotfiles directory
+echo "Changing to the ${dotfiledir} directory"
+cd ${dotfiledir}
+echo "...done"
+
+# create symlinks and overwrite old dotfiles
+for file in ${files}; do
+    echo "Creating symlink to $file in home directory."
+    ln -sf ${dotfiledir}/.${file} ${homedir}/.${file}
+done
+
+# Installting Mac tools/cLIs
 echo "Brew install from bundle file..."
 brew bundle install --file=Brewfile
